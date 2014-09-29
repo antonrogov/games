910cf3e4202db7e967483f1cbe5b51fb82e5fa7d
Anton Rogov <anton@blackbits.pro>
2014-09-30 02:03:08 +0400

    promoting the hex to the main app

--- game.js
+++ game.js
@@ -3,170 +3,97 @@ document.ontouchmove = function (e) {
 };
 
 window.onload = function () {
-  //start crafty
-  Crafty.init(50, 400, 320);
-  Crafty.canvas();
+  var canvas = document.getElementById('game'),
+	    ctx = canvas.getContext('2d'),
+      currentI = -1, currentJ = -1,
+      sqrt3 = 0.8660254037,
+      t = 30,
+      r = t * sqrt3,
+      gridX = 20 + r * 2,
+      gridY = 20 + t;
 
-  //turn the sprite map into usable components
-  Crafty.sprite(16, "sprite.png", {
-    grass1: [0,0],
-    grass2: [1,0],
-    grass3: [2,0],
-    grass4: [3,0],
-    flower: [0,1],
-    bush1: [0,2],
-    bush2: [1,2],
-    player: [0,3]
-  });
+  function drawHex(i, j, selected) {
+    ctx.strokeStyle = selected ? 'red' : 'grey';
+    ctx.lineWidth = 1;
+    ctx.beginPath();
 
-  //method to randomy generate the map
-  function generateWorld() {
-    //generate the grass along the x-axis
-    for(var i = 0; i < 25; i++) {
-      //generate the grass along the y-axis
-      for(var j = 0; j < 20; j++) {
-        grassType = Crafty.randRange(1, 4);
-        Crafty.e("2D, canvas, grass"+grassType)
-          .attr({x: i * 16, y: j * 16});
+    var x = gridX + (i - (j & 1) * 0.5) * r * 2,
+        y = gridY + j * t * 1.5;
 
-        //1/50 chance of drawing a flower and only within the bushes
-        if(i > 0 && i < 24 && j > 0 && j < 19 && Crafty.randRange(0, 50) > 49) {
-          Crafty.e("2D, DOM, flower, animate")
-            .attr({x: i * 16, y: j * 16})
-            .animate("wind", 0, 1, 3)
-            .bind("enterframe", function() {
-              if(!this.isPlaying())
-              this.animate("wind", 80);
-            });
-        }
+    ctx.moveTo(x, y - t);
+    ctx.lineTo(x + r, y - t / 2);
+    ctx.lineTo(x + r, y + t / 2);
+    ctx.lineTo(x, y + t);
+    ctx.lineTo(x - r, y + t / 2);
+    ctx.lineTo(x - r, y - t / 2);
+    ctx.closePath();
+    ctx.stroke();
+
+		ctx.fillStyle = "black"
+		ctx.font = "bolder 8pt Trebuchet MS,Tahoma,Verdana,Arial,sans-serif";
+		ctx.textAlign = "center";
+		ctx.textBaseline = 'middle';
+		ctx.fillText('(' + i + ',' + j + ')', x, y);
+  }
+
+  function render() {
+    ctx.clearRect(0, 0, 400, 300);
+    for (var i = 0; i < 5; i++) {
+      for (var j = 0; j < 4; j++) {
+        drawHex(i, j, i == currentI && j == currentJ);
       }
-    }
-
-    //create the bushes along the x-axis which will form the boundaries
-    for(var i = 0; i < 25; i++) {
-      Crafty.e("2D, canvas, wall_top, bush"+Crafty.randRange(1,2))
-        .attr({x: i * 16, y: 0, z: 2});
-      Crafty.e("2D, DOM, wall_bottom, bush"+Crafty.randRange(1,2))
-        .attr({x: i * 16, y: 304, z: 2});
-    }
-
-    //create the bushes along the y-axis
-    //we need to start one more and one less to not overlap the previous bushes
-    for(var i = 1; i < 19; i++) {
-      Crafty.e("2D, DOM, wall_left, bush"+Crafty.randRange(1,2))
-        .attr({x: 0, y: i * 16, z: 2});
-      Crafty.e("2D, canvas, wall_right, bush"+Crafty.randRange(1,2))
-        .attr({x: 384, y: i * 16, z: 2});
     }
   }
 
-  //the loading screen that will display while our assets load
-  Crafty.scene("loading", function() {
-    //load takes an array of assets and a callback when complete
-    Crafty.load(["sprite.png"], function() {
-      Crafty.scene("main"); //when everything is loaded, run the main scene
-    });
+  render();
 
-    //black background with some loading text
-    Crafty.background("#000");
-    Crafty.e("2D, DOM, text").attr({w: 100, h: 20, x: 150, y: 120})
-    .text("Loading")
-    .css({"text-align": "center"});
-  });
+  function select(x, y) {
+    x -= gridX;
+    y -= gridY;
 
-  //automatically play the loading scene
-  Crafty.scene("loading");
+    currentJ = (y + r) / (t * 1.5);
+    currentI = (x + r) / (r * 2) + (currentJ & 1) * 0.5;
 
-  Crafty.scene("main", function() {
-    generateWorld();
+    currentI = Math.floor(currentI);
+    currentJ = Math.floor(currentJ);
+    // currentI = (x * sqrt3 - y) / (t * 3);
+    // currentJ = (y * 2) / (t * 3);
 
-    Crafty.c('CustomControls', {
-      __move: {left: false, right: false, up: false, down: false},    
-      _speed: 3,
+    // var cx = currentI,
+    //     cz = currentJ,
+    //     cy = - cx - cz,
+    //     rx = Math.round(cx),
+    //     ry = Math.round(cy),
+    //     rz = Math.round(cz),
+    //     dx = Math.abs(cx - rx),
+    //     dy = Math.abs(cy - ry),
+    //     dz = Math.abs(cz - rz);
 
-      CustomControls: function(speed) {
-        if(speed) this._speed = speed;
-        var move = this.__move;
+    // if (dx > dy && dx > dz) {
+    //   rx = - ry - rz;
+    // } else if (dy > dz) {
+    //   ry = - rx - rz;
+    // } else {
+    //   rz = - rx - ry;
+    // }
 
-        this.bind('enterframe', function() {
-          //move the player in a direction depending on the booleans
-          //only move the player in one direction at a time (up/down/left/right)
-          if(move.right) this.x += this._speed; 
-          else if(move.left) this.x -= this._speed; 
-          else if(move.up) this.y -= this._speed;
-          else if(move.down) this.y += this._speed;
+    // currentI = rx;
+    // currentJ = rz + (rx + (rx & 1)) / 2;
 
-        }).bind('keydown', function(e) {
-          //default movement booleans to false
-          move.right = move.left = move.down = move.up = false;
+    render();
+  }
 
-          //if keys are down, set the direction
-          if(e.keyCode === Crafty.keys.RA) move.right = true;
-          if(e.keyCode === Crafty.keys.LA) move.left = true;
-          if(e.keyCode === Crafty.keys.UA) move.up = true;
-          if(e.keyCode === Crafty.keys.DA) move.down = true;
+  canvas.addEventListener('mousemove', function (e) {
+    select(e.pageX - canvas.offsetLeft,
+           e.pageY - canvas.offsetTop);
+  }, false);
 
-          this.preventTypeaheadFind(e);
-        }).bind('keyup', function(e) {
-          //if key is released, stop moving
-          if(e.keyCode === Crafty.keys.RA) move.right = false;
-          if(e.keyCode === Crafty.keys.LA) move.left = false;
-          if(e.keyCode === Crafty.keys.UA) move.up = false;
-          if(e.keyCode === Crafty.keys.DA) move.down = false;
+  canvas.addEventListener('touchmove', function (e) {
+    if (!e) return;
 
-          this.preventTypeaheadFind(e);
-        });
-
-        return this;
-      }
-    });
-
-    //create our player entity with some premade components
-    player = Crafty.e("2D, canvas, player, controls, CustomControls, animate, collision")
-                   .attr({x: 160, y: 144, z: 1})
-                   .CustomControls(1)
-                   .animate("walk_left", 6, 3, 8)
-                   .animate("walk_right", 9, 3, 11)
-                   .animate("walk_up", 3, 3, 5)
-                   .animate("walk_down", 0, 3, 2)
-                   .bind("enterframe", function(e) {
-                     if(this.__move.left) {
-                       if(!this.isPlaying("walk_left"))
-                         this.stop().animate("walk_left", 10);
-                     }
-
-                     if(this.__move.right) {
-                       if(!this.isPlaying("walk_right"))
-                         this.stop().animate("walk_right", 10);
-                     }
-
-                     if(this.__move.up) {
-                       if(!this.isPlaying("walk_up"))
-                         this.stop().animate("walk_up", 10);
-                     }
-
-                     if(this.__move.down) {
-                       if(!this.isPlaying("walk_down"))
-                         this.stop().animate("walk_down", 10);
-                     }
-                   })
-                   .bind("keyup", function(e) {
-                     this.stop();
-                   })
-                   .collision()
-                   .onhit("wall_left", function() {
-                     this.x += this._speed;
-                     this.stop();
-                   })
-                   .onhit("wall_right", function() {
-                     this.x -= this._speed;
-                     this.stop();
-                   }).onhit("wall_bottom", function() {
-                     this.y -= this._speed;
-                     this.stop();
-                   }).onhit("wall_top", function() {
-                     this.y += this._speed;
-                     this.stop();
-                   });
-  });
+    e.preventDefault();
+    select(e.targetTouches[0].pageX - canvas.offsetLeft,
+           e.targetTouches[0].pageY - canvas.offsetTop);
+  }, true);
 };
+
